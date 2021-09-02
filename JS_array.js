56972001847a203df566ba312e76caea432addf6
prati-shtha <70101462+prati-shtha@users.noreply.github.com>
2021-09-02 14:48:52 +0530

    Add files via upload

---
+++ JS_array.js
@@ -0,0 +1,7 @@
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+largeCountries.pop()
+largeCountries.push("Pakistan")
+largeCountries.shift()
+largeCountries.unshift("China")
+console.log(largeCountries)
+
